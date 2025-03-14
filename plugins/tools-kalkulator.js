22a7c8be0740ed9ff362befcefb98f7e025b255c
Branpedia <162459839+branpedia@users.noreply.github.com>
2025-03-15 05:03:21 +0700

    Update tools-kalkulator.js

--- plugins/tools-kalkulator.js
+++ plugins/tools-kalkulator.js
@@ -1,3 +1,9 @@
+/* Bran E-sport, V Kalkulator
+Saluran Wa : https://whatsapp.com/channel/0029VaR0ejN47Xe26WUarL3H
+IG: @bran_pedia | YT: Bran Pedia Official  
+Pembuat kode: Bran (Jangan dihapus, hargailah!)
+*/
+
 import { evaluate, format } from 'mathjs';
 
 let handler = async (m, { text }) => {
@@ -27,3 +33,36 @@ handler.tags = ['tools'];
 handler.command = /^(calc(ulate|ulator)?|kalk(ulator)?)$/i;
 
 export default handler;
+
+/* mendukung beberapa operator dan txt contoh
+
+Mendukung Beberapa Operator Silakan Cek Tabel Gambar ini 
+
+https://ar-hosting.pages.dev/1741989157130.jpg
+
+Contoh Penggunaan txt
+
+.calc 5+5
+📌 Hasil Perhitungan:
+5+5 = 10
+
+.calc 10÷2
+📌 Hasil Perhitungan:
+10÷2 = 5
+
+.calc 2^3
+📌 Hasil Perhitungan:
+2^3 = 8
+
+.calc sqrt(16)
+📌 Hasil Perhitungan:
+sqrt(16) = 4
+
+.calc log10(1000)
+📌 Hasil Perhitungan:
+log10(1000) = 3
+
+.calc sin(30 deg)
+📌 Hasil Perhitungan:
+sin(30 deg) = 0.5
+*/
