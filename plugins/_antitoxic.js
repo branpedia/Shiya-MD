1858c83de533a99ff7b5035cb4b27957c7a28f42
Branpedia <162459839+branpedia@users.noreply.github.com>
2025-04-06 17:53:48 +0700

    Update _antitoxic.js
    Fix checkin

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -2,24 +2,29 @@ let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?
 
 export function before(m, { isBotAdmin }) {
     if (m.isBaileys && m.fromMe) return !0
-    let chat = global.db.data.chats[m.chat]
-    let user = global.db.data.users[m.sender]
-    let isBadword = badwordRegex.exec(m.text)
-    console.log(isBadword)
+
+    let chat = global.db.data.chats[m.chat] || {}
+    let user = global.db.data.users[m.sender] || {}
+
+    // Inisialisasi jika belum ada
+    if (typeof user.warning !== 'number') user.warning = 0
+    if (typeof user.banned !== 'boolean') user.banned = false
+
+    let isBadword = badwordRegex.exec(m.text || '')
+    console.log('Badword detected:', isBadword)
 
     if (chat.antiBadword && isBadword) {
         user.warning += 1
-        m.reply('Jangan Toxic ya!!\n' + `kamu memiliki ${user.warning} warning` + '\nUntuk mematikan ketik *.disable antibadword*')
+        m.reply(`Jangan Toxic ya!!\nKamu memiliki ${user.warning} warning\nUntuk mematikan ketik *.disable antibadword*`)
+
         if (user.warning >= 5) {
             user.banned = false
             user.warning = 0
-            if (m.isGroup) {
-                if (isBotAdmin) {
-                    this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
-                    //this.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
-                }
+            if (m.isGroup && isBotAdmin) {
+                this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
             }
         }
     }
+
     return !0
 }
